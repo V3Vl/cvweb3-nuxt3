811768439127b1a0c74fe1bb2bae54f33bae0382
PeePeeDogO_o <1102200681@qq.com>
2023-11-03 02:19:47 +0800

    抽离unocss配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,42 @@
-import Components from "unplugin-vue-components/vite";
-import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineNuxtConfig({
-  modules: ["@unocss/nuxt", "@pinia/nuxt"],
+  modules: ['@unocss/nuxt', '@pinia/nuxt'],
   // 响应式转换，ref省略.value写法
   experimental: {
-    reactivityTransform: true,
+    reactivityTransform: true
   },
   vite: {
     plugins: [
       Components({
-        resolvers: [
-          AntDesignVueResolver({ importStyle: "css", resolveIcons: true }),
-        ],
-      }),
+        resolvers: [AntDesignVueResolver({ importStyle: 'css', resolveIcons: true })]
+      })
     ],
     css: {
       preprocessorOptions: {
         scss: {
-          javascriptEnabled: true,
-        },
-      },
-    },
-  },
-  unocss: {
-    uno: true,
-    attributify: true,
-    shortcuts: [
-      { flexc: "flex items-center justify-center" },
-      { bd: "border" },
-    ],
-    rules: [
-      [
-        /^fs-(\d+)$/,
-        ([, d]) => ({
-          "font-size": `${d}px`,
-        }),
-      ],
-      [/^p-(\d+)$/, (match) => ({ padding: `${match[1]}px` })],
-    ],
-  },
-});
+          javascriptEnabled: true
+        }
+      }
+    }
+  }
+  // 已抽离至unocss.config.ts
+  // unocss: {
+  //   uno: true,
+  //   attributify: true,
+  //   shortcuts: [
+  //     { flexc: "flex items-center justify-center" },
+  //     { bd: "border" },
+  //   ],
+  //   rules: [
+  //     [
+  //       /^fs-(\d+)$/,
+  //       ([, d]) => ({
+  //         "font-size": `${d}px`,
+  //       }),
+  //     ],
+  //     [/^p-(\d+)$/, (match) => ({ padding: `${match[1]}px` })],
+  //   ],
+  // },
+})
